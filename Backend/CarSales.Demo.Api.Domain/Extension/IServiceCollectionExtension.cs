050ceaba1c8b1d7bb0bc0cc1687a83b1b7775bc5
ajeet <ajeetx@email.com>
2020-08-03 00:00:15 +1000

    created library for domain logic

---
+++ Backend/CarSales.Demo.Api.Domain/Extension/IServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using CarSales.Demo.Api.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSales.Demo.Api.Domain.Extension
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddCustomServices(this IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(db => db.UseInMemoryDatabase("CarSalesDb"));
+            services.AddScoped<IVehicleService, VehicleService>();
+            services.AddScoped<IVehicleTableService, VehicleTableService>();
+            services.AddScoped<ICarService, CarService>();
+            services.AddScoped<IVehicleConverter, VehicleConverter>();
+            services.AddScoped<IVehicleDetailService, VehicleDetailService>();
+            return services;
+        }
+    }
+}
--- Backend/CarSales.Demo.Api/Startup.cs
+++ Backend/CarSales.Demo.Api/Startup.cs
@@ -1,17 +1,15 @@
-using CarSales.Demo.Api.Domain;
-using CarSales.Demo.Api.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 using Swashbuckle.AspNetCore.Swagger;
+using CarSales.Demo.Api.Domain.Extension;
 
 namespace CarSales.Demo.Api
 {
-public class Startup
+    public class Startup
     {
         public Startup(IConfiguration configuration)
         {
@@ -23,15 +21,9 @@ public class Startup
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
-
             services.AddMvc().AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver())
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContext<DataContext>(db => db.UseInMemoryDatabase("CarSalesDb"));
-            services.AddScoped<IVehicleService, VehicleService>();
-            services.AddScoped<IVehicleTableService, VehicleTableService>();
-            services.AddScoped<ICarService, CarService>();
-            services.AddScoped<IVehicleConverter, VehicleConverter>();
-            services.AddScoped<IVehicleDetailService, VehicleDetailService>();
+            services.AddCustomServices();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info
