b170e45674e8987b55d94fad1b8413c0eaeab4dc
ajeet kumar <ajeetx@email.com>
2020-09-08 12:02:56 +1000

    modified port and prod url

--- Backend/CarSales.Demo.Api/Program.cs
+++ Backend/CarSales.Demo.Api/Program.cs
@@ -11,7 +11,8 @@ namespace CarSales.Demo.Api
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args).UseUrls("http://localhost:3000")
+            WebHost.CreateDefaultBuilder(args)
+            // .UseUrls("http://localhost:3000")
                 .UseStartup<Startup>();
     }
 }
